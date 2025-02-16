4f4ec23e7cd8fbe3feac3cbc168281ac71a322c2
Прокофьев Александр <aleks.aleksp@gmail.com>
2025-02-16 23:45:12 +0400

    Added registration page

--- src/main/java/ru/prokofev/spring_security/controllers/AuthController.java
+++ src/main/java/ru/prokofev/spring_security/controllers/AuthController.java
@@ -2,7 +2,9 @@ package ru.prokofev.spring_security.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.prokofev.spring_security.models.Account;
 
 @Controller
 @RequestMapping("/auth")
@@ -11,5 +13,10 @@ public class AuthController {
     @GetMapping("/login")
     public String getLoginPage() {
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistrationPage(@ModelAttribute Account account) {
+        return "auth/registration";
     }
 }
